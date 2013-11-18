7e89310f92c224801d2916f950d26670d9add8e2
root <ricardo@themadvideo.com>
2013-11-18 12:20:35 +0100

    Checking isUtf8 function

--- src/Orderly/PayPalIpnBundle/Document/IpnOrders.php
+++ src/Orderly/PayPalIpnBundle/Document/IpnOrders.php
@@ -982,9 +982,9 @@ class IpnOrders
      */
     public function setAddressStreet($addressStreet)
     {
-        //if(!$this->isUtf8($addressStreet)){
+        if(!$this->isUtf8($addressStreet)){
             $addressStreet = $this->setToUtf8($addressStreet);
-        //}
+        }
         $this->addressStreet = $addressStreet;
     }
 
@@ -2532,10 +2532,17 @@ class IpnOrders
     }
     
     private function isUtf8($string) {
-        if(mb_detect_encoding($string) == "UTF-8"){
-            return true;
-        }
-        return false;
+        // From http://w3.org/International/questions/qa-forms-utf-8.html
+        return preg_match('%^(?:
+              [\x09\x0A\x0D\x20-\x7E]            # ASCII
+            | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+            |  \xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
+            | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+            |  \xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
+            |  \xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
+            | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+            |  \xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
+        )*$%xs', $string);
     }   
 
     private function setToUtf8($string) {
